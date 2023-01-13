bc12d2d6cd830fa559d5b657d0d12b87612324a4
John <jasonmcu@outlook.com>
2023-01-13 16:15:54 +0800

    c

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,7 @@ const persons = [
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'))
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
 );
